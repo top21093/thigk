cff39b5a8842796ecdcfd51bd0f16e95a94ea41c
LeDinhBaoTran <dh52007310@student.stu.edu.vn>
2023-11-13 13:29:02 +0700

    ktr so chinh phuong

--- source.cpp
+++ source.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int ktrSoChinhPhuong(int n) {
+	if ((int)sqrt(n) == sqrt(n)) {
+		return 1;
+	}
+	return 0;
+}
+
 int main() {
     int a;
     cout << "Nhap so nguyen a: ";
     cin >> a;
+    cout << ktrSoChinhPhuong(a) << endl;
     return 0;
 }
