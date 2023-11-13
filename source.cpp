9c19acd56d957ef23b85d1e4d52e4b19dc6993e3
LeDinhBaoTran <dh52007310@student.stu.edu.vn>
2023-11-13 13:27:46 +0700

    ktr hoan thien

--- source.cpp
+++ source.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int ktrSoHoanThien(int n) {
+	int s = 0;
+	for (int i =1; i<n; i++)
+		if (n %i == 0) {
+			s += i;
+		}
+	return s == n;
+}
+
 int main() {
-    int a;
-    cout << "Nhap so nguyen a: ";
-    cin >> a;
-    return 0;
+	int a;
+	cout << "Nhap so nguyen a: ";
+	cin >> a;
+	cout << ktrSoHoanThien(a);
+	return 0;
 }
